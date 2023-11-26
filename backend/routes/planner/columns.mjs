faffb265cdacbfc823a305260079e02fa1870d42
Faaez <razeenfaaez@gmail.com>
2023-11-26 10:02:42 -0600

    backenaedadadS

--- backend/routes/planner/columns.mjs
+++ backend/routes/planner/columns.mjs
@@ -4,18 +4,22 @@ import { errorHandler, getUsername } from '../../middleware/index.mjs'
 
 const router = express.Router()
 
-// Add new column to board TODO:
-router.post('/planner/boards/:boardId/columns', async (req, res) => {
-  try {
-    const { boardId } = req.params
-    const { newColumnId, newColumnName } = req.body
-  } catch (error) {
-    console.error(error)
-    res.status(500).send('Internal Server Error')
-  }
-})
+const addNewColumn = async (req, res) => {
+  const username = getUsername(req)
+  const { boardId } = req.params
+  const { newColumnDetails: newColumn, updatedColumns } = req.body
+  await db.collection('planner').updateOne(
+    { username },
+    {
+      $set: {
+        [`boards.${boardId}.columns`]: updatedColumns,
+        [`columns.${newColumn.id}`]: newColumn,
+      },
+    }
+  )
+  res.status(201).end()
+}
 
-// Reorder column within board
 const changeColumnOrder = async (req, res) => {
   const username = getUsername(req)
   const { boardId } = req.params
@@ -26,8 +30,28 @@ const changeColumnOrder = async (req, res) => {
       { username, [`boards.${boardId}.id`]: boardId },
       { $set: { [`boards.${boardId}.columns`]: newColumnOrder } }
     )
+  res.status(204).end()
 }
 
+const deleteColumn = async (req, res) => {
+  const username = getUsername(req)
+  const { boardId, columnId } = req.params
+  await db.collection('planner').updateOne(
+    { username },
+    {
+      $pull: {
+        [`boards.${boardId}.columns`]: columnId,
+      },
+      $unset: {
+        [`columns.${columnId}`]: 1,
+      },
+    }
+  )
+  res.status(204).end()
+}
+
+router.post('/planner/boards/:boardId/columns', errorHandler(addNewColumn))
 router.patch('/planner/boards/:boardId/columns/reorder', errorHandler(changeColumnOrder))
+router.delete('/planner/boards/:boardId/columns/:columnId/delete', errorHandler(deleteColumn))
 
 export default router
