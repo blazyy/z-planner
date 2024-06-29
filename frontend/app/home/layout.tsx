d4981c0d67e1fbf9d57c2d0cf6a092d3efb1aa39
Faaez <razeenfaaez@gmail.com>
2024-06-29 14:12:01 -0500

    Rename component

--- frontend/app/home/layout.tsx
+++ frontend/app/home/layout.tsx
@@ -2,7 +2,7 @@ import { Navbar } from '@/components/global/Navbar'
 import { cn } from '@/lib/utils'
 import { quicksand } from '../layout'
 
-export default function RootLayout({ children }: { children: JSX.Element }) {
+export default function Layout({ children }: { children: JSX.Element }) {
   return (
     <div className={cn(quicksand.className, 'flex flex-col min-h-screen')}>
       <Navbar />
