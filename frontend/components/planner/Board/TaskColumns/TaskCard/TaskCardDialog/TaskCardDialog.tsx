6490741d83277c3be2b067c4251d804da7785f0b
Faaez <razeenfaaez@gmail.com>
2024-06-28 21:59:38 -0500

    Card title and content dispatches

--- frontend/components/planner/Board/TaskColumns/TaskCard/TaskCardDialog/TaskCardDialog.tsx
+++ frontend/components/planner/Board/TaskColumns/TaskCard/TaskCardDialog/TaskCardDialog.tsx
@@ -1,14 +1,14 @@
-import changeCardContent from '@/app/utils/plannerUtils/cardUtils/changeCardContent'
-import changeCardTitle from '@/app/utils/plannerUtils/cardUtils/changeCardTitle'
 import { Card, CardContent, CardFooter, CardHeader, CardTitle } from '@/components/ui/card'
 import { Checkbox } from '@/components/ui/checkbox'
 import { DialogContent } from '@/components/ui/dialog'
 import { Separator } from '@/components/ui/separator'
 import { Textarea } from '@/components/ui/textarea'
+import { DEBOUNCE_TIME_MS } from '@/constants/constants'
 import { usePlanner, usePlannerDispatch } from '@/hooks/Planner/Planner'
 import { useAuth } from '@clerk/nextjs'
 import { useMutation } from '@tanstack/react-query'
 import axios from 'axios'
+import debounce from 'lodash/debounce'
 import { useErrorBoundary } from 'react-error-boundary'
 import { CategoryBadge } from '../CategoryBadge'
 import { EditableSubTasks } from './EditableSubTasks/EditableSubTasks'
@@ -17,6 +17,22 @@ type TaskCardDialogProps = {
   boardId: string
   id: string
 }
+
+const debouncedPatchTitle = debounce(async (taskCardId, newTitle, token) => {
+  await axios.patch(
+    `${process.env.NEXT_PUBLIC_BACKEND_URL}/planner/cards/${taskCardId}/title`,
+    { newTitle },
+    { headers: { Authorization: `Bearer ${token}` } }
+  )
+}, DEBOUNCE_TIME_MS)
+
+const debouncedPatchContent = debounce(async (taskCardId, newContent, token) => {
+  await axios.patch(
+    `${process.env.NEXT_PUBLIC_BACKEND_URL}/planner/cards/${taskCardId}/content`,
+    { newContent },
+    { headers: { Authorization: `Bearer ${token}` } }
+  )
+}, DEBOUNCE_TIME_MS)
 
 export const TaskCardDialog = ({ boardId, id }: TaskCardDialogProps) => {
   const { showBoundary } = useErrorBoundary()
@@ -54,6 +70,44 @@ export const TaskCardDialog = ({ boardId, id }: TaskCardDialogProps) => {
     },
   })
 
+  const changeCardTitleMutation = useMutation({
+    mutationFn: async (payload: any) => {
+      const token = await getToken()
+      const { taskCardId, newTitle } = payload
+      return debouncedPatchTitle(taskCardId, newTitle, token)
+    },
+    onMutate: async (payload) => {
+      dispatch({
+        type: 'taskCardTitleChanged',
+        payload: payload,
+      })
+    },
+    onError: (err) => {
+      dispatch({
+        type: 'backendErrorOccurred',
+      })
+    },
+  })
+
+  const changeCardContentMutation = useMutation({
+    mutationFn: async (payload: any) => {
+      const token = await getToken()
+      const { taskCardId, newContent } = payload
+      return debouncedPatchContent(taskCardId, newContent, token)
+    },
+    onMutate: async (payload) => {
+      dispatch({
+        type: 'taskCardContentChanged',
+        payload: payload,
+      })
+    },
+    onError: (err) => {
+      dispatch({
+        type: 'backendErrorOccurred',
+      })
+    },
+  })
+
   return (
     <DialogContent className='p-0'>
       <Card className='p-2'>
@@ -78,7 +132,13 @@ export const TaskCardDialog = ({ boardId, id }: TaskCardDialogProps) => {
                 <Textarea
                   value={task.title}
                   className='items-center p-0 border-none h-[35px] text-2xl focus-visible:ring-0 focus-visible:ring-transparent resize-y'
-                  onChange={(event) => changeCardTitle(id, event.target.value, dispatch, showBoundary)}
+                  onChange={(event) => {
+                    const payload = {
+                      taskCardId: id,
+                      newTitle: event.target.value,
+                    }
+                    changeCardTitleMutation.mutate(payload)
+                  }}
                 />
               </div>
             </div>
@@ -91,7 +151,13 @@ export const TaskCardDialog = ({ boardId, id }: TaskCardDialogProps) => {
             placeholder='Notes...'
             value={task.content}
             className='bg-neutral-100 m-1 min-h-fit focus-visible:ring-0 focus-visible:ring-transparent resize-y'
-            onChange={(event) => changeCardContent(id, event.target.value, dispatch, showBoundary)}
+            onChange={(event) => {
+              const payload = {
+                taskCardId: id,
+                newContent: event.target.value,
+              }
+              changeCardContentMutation.mutate(payload)
+            }}
           />
         </CardContent>
         <CardFooter className='flex justify-between'>{/* <CategoryBadge taskCardId={id} /> */}</CardFooter>
